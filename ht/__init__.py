79694f22041d6eee0f7aa21780293ae7f1500200
mirnovov <anon185441@gmail.com>
2021-05-24 08:54:49 +0000

    Improve metadata, and remove useless and redundant stuff that is only for PyPl

--- ht/__init__.py
+++ ht/__init__.py
@@ -1,1 +1,9 @@
-from .version import __version__
+from collections import namedtuple
+
+__version__ = "0.1"
+__author__ = "novov"
+__license__ = "MIT"
+__copyright__ = "(c) 2021 novov"
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro") 
+version_info = VersionInfo(major = 0, minor = 1, micro = 0)
