8c598f18642c4972672c6bed06be3e80b4d29e24
mirnovov <anon185441@gmail.com>
2021-05-28 04:54:21 +0000

    Increment version

--- ht/__init__.py
+++ ht/__init__.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
 
-__version__ = "4.0.1"
+__version__ = "4.0.2"
 __author__ = "novov"
 __license__ = "MIT"
 __copyright__ = "(c) 2021 novov"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro qualified") 
-version_info = VersionInfo(major = 4, minor = 0, micro = 1, qualified = "final")
+version_info = VersionInfo(major = 4, minor = 0, micro = 2, qualified = "final")
