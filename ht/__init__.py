249de21ccbcc9e63364d323317facaa1a69a77f2
mirnovov <anon185441@gmail.com>
2021-08-28 11:03:12 +0000

    Remove alpha status

--- ht/__init__.py
+++ ht/__init__.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
 
-__version__ = "4.4.0 alpha"
+__version__ = "4.4.0"
 __author__ = "novov"
 __license__ = "MIT"
 __copyright__ = "(c) 2021 novov"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro qualified") 
-version_info = VersionInfo(major = 4, minor = 4, micro = 0, qualified = "alpha")
+version_info = VersionInfo(major = 4, minor = 4, micro = 0, qualified = "final")
