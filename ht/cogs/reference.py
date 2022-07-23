6136a2023784d383c9847265844c80931dc4433f
mirnovov <anon185441@gmail.com>
2022-07-23 04:42:13 +0000

    Add hatching to tinctures command

--- ht/cogs/reference.py
+++ ht/cogs/reference.py
@@ -137,13 +137,17 @@ class HeraldryReference(utils.MeldedCog, name = "Reference", category = "Heraldr
 	async def tinctures(self, ctx):
 		await ctx.send(
 			"In heraldry, *tinctures* are divided into:\n\n"
-			"- **Colours**: :red_circle: Gules, :green_circle: Vert,"
-			" :blue_circle: Azure, :black_circle: Sable, :purple_circle: Purpure\n\n"
-			"- **Metals**: :yellow_circle: Or, :white_circle: Argent\n\n"
-			"- **Furs**: Ermine, Vair, etc.\n\nThe *rule of tincture* states that colour"
-			" can only touch metal, except for minor details and field divisions; furs can touch both."
-			" Keep in mind that the exact shades can vary. Various rarer tinctures also exist,"
-			" but many dislike them; use with caution."
+			"- **Colours**: :red_circle:<:hatching_gules:1000259867333906483> Gules,"
+			" :green_circle:<:hatching_vert:1000259880122335342> Vert,"
+			" :blue_circle:<:hatching_azure:1000259865471631370> Azure,"
+			" <:sable:1000259882450157739><:hatching_sable:1000259875726688366> Sable,"
+			" :purple_circle:<:hatching_purpure:1000259873348526201> Purpure\n\n"
+			"- **Metals**: :yellow_circle:<:hatching_or:1000259870211194890> Or,"
+			" :white_circle::white_circle: Argent\n\n"
+			"- **Furs**: <:ermine:1000259861482840154> Ermine, <:vair:1000259886036299819> Vair,"
+			" etc.\n\nThe *rule of tincture* states that colour can only touch metal, except for"
+			" minor details and field divisions; furs can touch both. Keep in mind that the exact"
+			" shades can vary. Various rarer tinctures also exist, but many dislike them; use with caution."
 		)
 
 def setup(bot):
