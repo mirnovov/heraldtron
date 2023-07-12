a749bd9c08d0634f2c875d4034e25ed0d25ecafe
novov <anon185441@gmail.com>
2023-07-12 14:36:05 +1200

    Trim symbolism to stop it from breaking embed limits

--- ht/cogs/roll.py
+++ ht/cogs/roll.py
@@ -81,7 +81,7 @@ class HeraldryRoll(utils.MeldedCog, name = "Roll of Arms", category = "Heraldry"
 				symbolism_text += f"{markdown}\n"
 				next_section = next_section.next_sibling
 
-			embed = embeds.GENERIC.create(f"Symbolism for {self.format_armiger(user)}", symbolism_text, heading = f"GreiiN:{user[0]:04}")
+			embed = embeds.GENERIC.create(f"Symbolism for {self.format_armiger(user)}", symbolism_text[:4095], heading = f"GreiiN:{user[0]:04}")
 			embed.set_footer(text = "Textual content from https://roll-of-arms.com by GreiiEquites.")
 
 			await ctx.send(embed = embed)
